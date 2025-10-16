37fac3610141a8bfe4fd9195ac86bd22e7ae2843
GifuKyoRitsu <kii@gku.ac.jp>
2025-10-16 15:36:08 +0900

    v06

--- daisho-day1/script.js
+++ daisho-day1/script.js
@@ -2,12 +2,12 @@ const helloBtn=document.getElementByld("helloBtn")
 const message=document.getelementbyld("message")
 
 helloBtn.addeventlistener("click",()=>{
-    const hour=newDate().gethours();
-    let greeting="こんにちは！"
+    const hour=newDate().getHours();
+    let greeting="こんにちは！";
     if (hour<12){
         greeting="おはようございます！";
     }else if(hour>=18){
-        greeting="こんばんは！"
+        greeting="こんばんは！";
     }
     message.textcontent=`${greeting}GitHub Pages の公開に成功しました！`;
 });
